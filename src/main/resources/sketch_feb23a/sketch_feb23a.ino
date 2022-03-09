c0395ff70cb69dcb96ee5f9cdc94180cc5344bf9
Gamebuster19901 <Gamebuster19901@users.noreply.github.com>
2022-03-09 02:29:26 -0500

    rm unnecessary code

--- src/main/resources/sketch_feb23a/sketch_feb23a.ino
+++ src/main/resources/sketch_feb23a/sketch_feb23a.ino
@@ -3,31 +3,20 @@
 SoftwareSerial BTserial(0,1);
 
 int baud = 9600;
-int PulseSensorPurplePin = 0;// Pulse Sensor PURPLE WIRE connected to ANALOG PIN 0
-int LED13 = 13; //The on-board Arduio LED
+int PulseSensorPin = 0;// Pulse Sensor PURPLE WIRE connected to ANALOG PIN 0
 int Signal;// holds the incoming raw data. Signal value can range from 0-1024
-int Threshold = 550;// Determine which Signal to "count as a beat", and which to ingore.
 
 
 void setup() {
-  pinMode(LED13,OUTPUT); // pin that will blink to heartbeat!
-  Serial.begin(baud);
+  pinMode(13, OUTPUT);
+  digitalWrite(13,LOW); //Turn off LED.
   BTserial.begin(baud);
 }
 
 void loop() {
 
-  Signal = analogRead(PulseSensorPurplePin);// Read the PulseSensor's value.
-
-  //Serial.println(Signal);// Send the Signal value to Serial Plotter.
-  BTserial.println(Signal);//Send the Signal over bluetooth
-
-
-  if(Signal > Threshold){ //Turn on Arduino's on-Board LED.
-    digitalWrite(LED13,HIGH);
-  } else {
-    digitalWrite(LED13,LOW); //Turn off LED.
-  }
+  Signal = analogRead(PulseSensorPin);
+  BTserial.println(Signal);
   
   delay(10);
 }
