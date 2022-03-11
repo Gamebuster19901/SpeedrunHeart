bb55f13bf51160fd2c6057aa3d758f23c4bb735c
Gamebuster19901 <Gamebuster19901@users.noreply.github.com>
2022-03-11 18:44:42 -0500

    Add gamebuster theme colors

--- src/main/java/com/gamebuster19901/heart/Main.java
+++ src/main/java/com/gamebuster19901/heart/Main.java
@@ -1,6 +1,17 @@
 package com.gamebuster19901.heart;
 
+import java.awt.BasicStroke;
+import java.awt.Color;
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import javax.swing.Timer;
+
+import org.jfree.chart.plot.ValueMarker;
+import org.jfree.chart.ui.Layer;
+import org.jfree.chart.ui.RectangleAnchor;
+import org.jfree.chart.ui.TextAnchor;
 
 import com.gamebuster19901.heart.gui.Monitor;
 import com.gamebuster19901.heart.gui.MonitorWorker;
@@ -9,6 +20,9 @@ import jssc.SerialPortException;
 
 public class Main {
 
+	public static final Color GBColor = Color.decode("0x7F3FA5");
+	private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+	
 	public static void main(String[] args) throws InterruptedException, SerialPortException, IOException {
 		PulseDevice pulseDevice = PulseDevice.tryGetHC06("/dev/ttyACM1", 10, true);
 		
@@ -16,6 +30,19 @@ public class Main {
 		MonitorWorker worker = new MonitorWorker(monitor);
 		pulseDevice.addListener(worker);
 		worker.execute();
+		Timer timer = new Timer(1000, (e) -> {
+			LocalDateTime now = LocalDateTime.now();
+			ValueMarker marker = new ValueMarker(monitor.getChart().getXYPlot().getDomainAxis(0).getUpperBound());
+			marker.setLabelTextAnchor(TextAnchor.BOTTOM_CENTER);
+			marker.setStroke(new BasicStroke(2));
+			marker.setLabelAnchor(RectangleAnchor.BOTTOM);
+			marker.setLabelBackgroundColor(Color.BLACK);
+			marker.setLabelPaint(GBColor);
+			marker.setLabelFont(marker.getLabelFont().deriveFont(20f));
+			marker.setLabel(formatter.format(now));
+			monitor.getChart().getXYPlot().addDomainMarker(0, marker, Layer.BACKGROUND);
+		});
+		timer.start();
 		
 		
 		//while(true) {
--- src/main/java/com/gamebuster19901/heart/gui/Monitor.java
+++ src/main/java/com/gamebuster19901/heart/gui/Monitor.java
@@ -1,17 +1,24 @@
 package com.gamebuster19901.heart.gui;
 
+import java.awt.BasicStroke;
+import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.geom.Rectangle2D;
 import java.io.IOException;
+import java.lang.reflect.Field;
 
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
 import org.jfree.chart.plot.XYPlot;
+import org.jfree.chart.renderer.category.LineAndShapeRenderer;
 import org.jfree.chart.ui.ApplicationFrame;
+import org.jfree.chart.ui.RectangleInsets;
 import org.jfree.data.Range;
 import org.jfree.data.time.DynamicTimeSeriesCollection;
-import org.jfree.data.time.Millisecond;
+import org.jfree.data.time.Second;
 
+import com.gamebuster19901.heart.Main;
 import com.gamebuster19901.heart.PulseDevice;
 import com.gamebuster19901.heart.PulseDevice.PulseListener;
 
@@ -26,16 +33,30 @@ public class Monitor extends ApplicationFrame implements PulseListener {
 	public Monitor(PulseDevice pulseDevice) throws IOException, InterruptedException {
 		super("test");
 		this.pulseDevice = pulseDevice;
-		dataset = new DynamicTimeSeriesCollection(1, 2000, new Millisecond());
-		dataset.setTimeBase(new Millisecond());
+		dataset = new DynamicTimeSeriesCollection(1, 2000, new Second());
+		dataset.setTimeBase(new Second());
 		dataset.addSeries(new float[] {}, 0, "Refractive Index");
 		chart = ChartFactory.createTimeSeriesChart("", "", "", dataset, false, false, false);
-		//chart.setNotify(false);
-		final XYPlot plot = chart.getXYPlot();
-		plot.getDomainAxis().setAutoRange(true);
-		plot.getRangeAxis().setRange(new Range(0, 1024), false, true);
+		chart.setBorderVisible(false);
 		
-		this.add(new ChartPanel(chart));
+		final XYPlot plot = chart.getXYPlot();
+		plot.setBackgroundPaint(Color.BLACK);
+		plot.setRangeGridlinesVisible(false);
+		plot.setDomainGridlinesVisible(false);
+		plot.getDomainAxis().setAutoRange(true);
+		plot.getDomainAxis().setVisible(false);
+		plot.getRangeAxis().setRange(new Range(0, 1024), false, true);
+		plot.getRangeAxis().setVisible(false);
+		plot.setOutlinePaint(null);
+		plot.setInsets(new RectangleInsets() {
+			public void trim(Rectangle2D area) {}
+		});
+		
+		plot.getRenderer().setSeriesStroke(0, new BasicStroke(3));
+		plot.getRenderer().setSeriesPaint(0, Main.GBColor);
+		
+		ChartPanel panel = new ChartPanel(chart);
+		this.add(panel);
 		setVisible(true);
 	}
 	
